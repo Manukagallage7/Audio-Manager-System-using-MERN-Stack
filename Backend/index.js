ddeb3eb9706f026eb89234930a94844ff4ce0981
Manukagallage7 <manukakavinda1110@gmail.com>
2026-01-02 23:38:26 +0530

    Add JWT authentication middleware and fix product router path

--- Backend/index.js
+++ Backend/index.js
@@ -2,11 +2,27 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import userRouter from './src/routes/userRouter.js';
+import productRouter from './src/routes/productRouter.js';
 import 'dotenv/config';
+import jwt from 'jsonwebtoken';
 
 let app = express();
 
 app.use(bodyParser.json())
+app.use((req, res, next)=> {
+    const token = req.header("Authorization")
+    if(token!== null){
+        token = token.replace("Bearer", "")
+
+        jwt.verify(token, "Audio-Manager-123",(err, decoded)=> {
+            if(!err){
+                req.user = decoded
+
+            }
+        })
+    }
+    next()
+})
 
 const mongoUrl = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/audio-manager';
 
@@ -20,6 +36,7 @@ mongoose.connect(mongoUrl)
 let connection = mongoose.connection;
 
 app.use("/api/users", userRouter);
+app.use("api/product", productRouter)
 
 app.listen(5000, ()=> {
         console.log("Server is running on port 5000");
