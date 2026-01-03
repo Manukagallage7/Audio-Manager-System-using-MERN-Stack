a23022f93577d4f6d57808c1b891a8b18a243207
Manukagallage7 <manukakavinda1110@gmail.com>
2026-01-03 13:03:39 +0530

    Add review model with schema definition for reviews

---
+++ Backend/src/models/reviewModel.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+const reviewSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true
+    },
+    comment: {
+        type:String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true,
+        default: Date.now()
+    },
+    isApproved: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    profilePicture: {
+        type: String,
+        required: true,
+        default: ""
+    }
+})
+
+const reviewModel = mongoose.model("Review", reviewSchema)
+
+export default reviewModel;
