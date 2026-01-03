59e2bc922fcf9be89d5b9efdc8f88bf1cabdb17a
Manukagallage7 <manukakavinda1110@gmail.com>
2026-01-03 13:03:27 +0530

    Add review controller with CRUD operations and user authorization

---
+++ Backend/src/controllers/reviewController.js
@@ -0,0 +1,166 @@
+import reviewModel from "../models/reviewModel.js";
+
+export function addReview(req,res) {
+
+    if(req.user == null){
+        res.status(401).json({
+            message: "Please login & try again"
+        })
+        return
+    }
+
+    const data = req.body
+
+    data.name = req.user.firstName + " " + req.user.lastName
+    data.profilePicture = req.user.profilePicture
+    data.email = req.user.email
+
+    const newReview = new reviewModel(data)
+
+    newReview.save()
+    .then(()=> {
+        res.status(201).json({
+            message: "Review added successfully"
+        })
+    })
+    .catch((err)=> {
+        res.status(500).json({
+            error: "Review addition failed"
+        })
+    })
+
+}
+
+export function getReviews(req,res) {
+    const user = req.user
+    if(user == null || user.type !== "admin") {
+        reviewModel.find({
+            isApproved: true
+        })
+        .then((reviews)=> {
+            res.json(reviews)
+        })
+        return
+    }
+
+    if(user.type == "admin") {
+        reviewModel.find()
+        .then((reviews)=> {
+            res.json(reviews)
+        })
+    }
+}
+
+export function updateReview(req,res) {
+    const email = req.params.email
+
+    if(req.user == null){
+        res.status(401).json({
+            message: "Please login & try again"
+        })
+        return
+    }
+
+    if(req.user.type !== "admin"){
+    reviewModel.updateOne(
+        {email: email},
+        { $set: update }
+    ).then(()=>{
+        res.json({
+            message: "Review update successfully"
+        })
+    }).catch(()=> {
+        res.status(500).json({
+            error: "Review updating failed"
+        })
+    })
+    return
+    }
+
+    if(req.user.type === "customer") {
+        if(req.user.email === email) {
+            reviewModel.updateOne(
+                {email: email},
+                { $set: update }
+            ).then(()=>{
+                res.json({
+                    message: "Review updated successfully"
+                })
+            }).catch(()=> {
+                res.status(500).json({
+                    error: "Review updating failed"
+                })
+            })
+        }
+    }
+}
+
+export function deleteReview(req,res) {
+    const email = req.params.email
+
+    if(req.user == null){
+        res.status(401).json({
+            message: "Please login & try again"
+        })
+        return
+    }
+
+    if(req.user.type == "admin"){
+        reviewModel.deleteOne({
+        email: email
+    }).then(()=>{
+        res.json({
+            message: "Review deleted successfully"
+        })
+    }).catch(()=> {
+        res.status(500).json({
+            error: "Review deletion failed"
+        })
+    })
+    return
+    }
+
+    if(req.user.type == "customer") {
+        if(req.user.email == email) {
+            reviewModel.deleteOne({
+                email: email
+            }).then(()=>{
+                res.json({
+                    message: "Review deleted successfully"
+                })
+            }).catch(()=> {
+                res.status(500).json({
+                    error: "Review deletion failed"
+                })
+            })
+        }
+    }
+}
+
+export function approveReview(req,res) {
+    const email = req.params.email
+
+    if(req.user == null || req.user.type !== "admin"){
+        res.status(401).json({
+            message: "Please login as admin & try again"
+        })
+        return
+    }
+
+    reviewModel.updateOne(
+        {
+            email: email
+        },
+        {
+            isApproved: true
+        }
+    ).then(()=>{
+        res.json({
+            message: "Review approved successfully"
+        })
+    }).catch(()=> {
+        res.status(500).json({
+            error: "Review approval failed"
+        })
+    })
+}
