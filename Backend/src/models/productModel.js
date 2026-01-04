e8c15b9cc2600f8b79de4824b2fcdebaeb4ba14b
Manukagallage7 <manukakavinda1110@gmail.com>
2026-01-04 23:15:13 +0530

    Add key field to product schema with unique constraint

--- Backend/src/models/productModel.js
+++ Backend/src/models/productModel.js
@@ -1,12 +1,36 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
+    key: {
+        type: String,
+        required: true,
+        unique: true
+    },
     name: {
         type: String,
         required: true
     },
     price: {
-
+        type: Number,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true,
+        default: "uncategorized"
+    },
+    dimensions: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    availability: {
+        type: Boolean,
+        required: true,
+        default: true
     }
 })
 
