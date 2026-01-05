791e95815c86730fb83e16c0b20a2151750be26a
Manukagallage7 <manukakavinda1110@gmail.com>
2026-01-05 13:32:59 +0530

    Add inquiry model with schema for managing inquiries

---
+++ Backend/src/models/inquiryModel.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+
+const inquirySchema = new mongoose.Schema({
+    id: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    response: {
+        type: String,
+        required: true,
+        default: "Pending"
+    },
+    isResolved: {
+        type: Boolean,
+        required: true,
+        default: false
+    }
+})
+
+const inquiryModel = mongoose.model('Inquiry', inquirySchema)
+
+export default inquiryModel;
+    
