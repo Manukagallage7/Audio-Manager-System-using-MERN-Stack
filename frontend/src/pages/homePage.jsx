e52e510190c5f3a955b2ad92b46a61621d30b7d0
Manukagallage7 <manukakavinda1110@gmail.com>
2026-01-07 17:21:08 +0530

    add HomePage component with routing and navigation

---
+++ frontend/src/pages/homePage.jsx
@@ -0,0 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
+import Header from '../components/header.jsx';
+import Items from '../home/items.jsx';
+import Gallery from '../home/gallery.jsx';
+import Contact from '../home/contact.jsx';
+import About from '../home/about.jsx';
+import Home from '../home/home.jsx';
+
+export default function HomePage() {
+    return (
+        <>
+            <Header />
+            <div className='h-[calc(100vh-100px)] w-full'>
+                <Routes path="/*">
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/items" element={<Items />} />
+                    <Route path="/gallery" element={<Gallery />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="*" element={<h1>404: Page Not Found</h1>} />
+                </Routes>
+            </div>
+        </>
+    )
+}
