54bfd253e7f8df99beafa6239757c6c62cbff8b2
Manukagallage7 <manukakavinda1110@gmail.com>
2026-01-07 17:20:14 +0530

    add Contact component for the Contact page

---
+++ frontend/src/home/contact.jsx
@@ -0,0 +1,7 @@
+export default function Contact() {
+    return (
+        <div className="w-full h-full flex justify-center items-center">
+            <h1 className="text-[40px] font-bold">Contact Page</h1>
+        </div>
+    )
+}
