879acc827474a0273e396190b311a1b682f5d65f
Manukagallage7 <manukakavinda1110@gmail.com>
2026-01-09 18:08:53 +0530

    add Toaster component and update routing for LoginPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import { Toaster } from 'react-hot-toast';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import AdminPage from './pages/adminPage';
 import HomePage from './pages/homePage';
+import LoginPage from './user/loginPage';
 
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Toaster position='top-right' />
       <Routes path="/">
         <Route path="/adminPage/*" element={<AdminPage />} />
         <Route path="/homePage/*" element={<HomePage />} />
+        <Route path="/loginPage" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   )
