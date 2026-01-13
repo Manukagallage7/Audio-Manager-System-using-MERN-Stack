cbe1b46bf3948bc4b8af5e7aa2b44674ef532e59
Manukagallage7 <manukakavinda1110@gmail.com>
2026-01-13 18:59:05 +0530

    add getProduct function to productRouter for fetching a product by key

--- Backend/src/routes/productRouter.js
+++ Backend/src/routes/productRouter.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import {addProduct, deleteProduct, getProducts, updateProduct} from '../controllers/productController.js';
+import {addProduct, deleteProduct, getProduct, getProducts, updateProduct} from '../controllers/productController.js';
 
 const productRouter = express.Router();
 
 productRouter.post('/', addProduct)
 productRouter.get('/', getProducts)
+productRouter.get('/:key', getProduct)
 productRouter.put('/:key', updateProduct)
 productRouter.delete('/:key', deleteProduct)
 
