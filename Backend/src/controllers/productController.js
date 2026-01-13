63fd0a1dc879727802b7f5ceb1379934150fe2b3
Manukagallage7 <manukakavinda1110@gmail.com>
2026-01-13 18:58:59 +0530

    add getProduct and getProductByKey functions for fetching products by key

--- Backend/src/controllers/productController.js
+++ Backend/src/controllers/productController.js
@@ -53,6 +53,52 @@ export async function getProducts(req, res) {
     }
 }
 
+export async function getProduct(req, res) {
+    try {
+        if(isAdmin(req)){
+        const key = req.params.key;
+        const product = await Product.findOne({ key });
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+        res.status(200).json({ message: 'Product fetched successfully', product });
+    } else {
+        res.status(403).json({ message: 'You are Unauthorized access' });
+        return
+    }
+    } catch (err) {
+        res.status(500).json({ message: 'Failed to fetch product' });
+    }
+}
+
+export async function getProductByKey(req, res) {
+    try {
+        if(isAdmin(req)){
+            const key = req.params.key
+        const product = await Product.findOne({ key })
+        if (product) {
+            res.status(200).json({
+                message: "Product fetched successfully",
+                product
+            })
+        } else {
+            res.status(404).json({
+                message: "Product not found"
+            })
+        }
+        } else {
+            res.status(403).json({
+                message: "You are Unauthorized access"
+            })
+            return
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "Failed to fetch product"
+        })
+    }
+}
+
 export async function updateProduct(req, res) {
     try {
         if (isAdmin(req)) {
@@ -76,8 +122,6 @@ export async function updateProduct(req, res) {
         })
     }
 }
-    
-    
 
 export async function deleteProduct(req, res) {
     try {
