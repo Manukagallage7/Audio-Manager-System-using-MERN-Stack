6d3cbd5289e316a4ff0434e6b73b706ec0eb0aef
Manukagallage7 <manukakavinda1110@gmail.com>
2026-01-15 22:51:29 +0530

    feat: add user management routes for retrieving and blocking/unblocking users

--- Backend/src/routes/userRouter.js
+++ Backend/src/routes/userRouter.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { registerUser, loginUser } from '../controllers/userController.js';
+import { registerUser, loginUser, getAllUsers, blockUser, unblockUser } from '../controllers/userController.js';
 
 const userRouter = express.Router();
 
 userRouter.post('/register', registerUser);
 userRouter.post('/login', loginUser);
+userRouter.get('/users', getAllUsers);
+userRouter.put('/users/block/:email', blockUser);
+userRouter.put('/users/unblock/:email', unblockUser);
 
 export default userRouter;
