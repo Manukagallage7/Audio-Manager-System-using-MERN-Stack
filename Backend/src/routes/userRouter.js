ff3fc8397fd6458f252a9a33d2d88419e4a46f68
Manukagallage7 <manukakavinda1110@gmail.com>
2026-02-08 00:44:50 +0530

    feat: add verify OTP route to userRouter for email verification

--- Backend/src/routes/userRouter.js
+++ Backend/src/routes/userRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registerUser, loginUser, getAllUsers, blockUser, unblockUser, sendOTP, googleLogin } from '../controllers/userController.js';
+import { registerUser, loginUser, getAllUsers, blockUser, unblockUser, sendOTP, verifyOTP, googleLogin } from '../controllers/userController.js';
 
 const userRouter = express.Router();
 
@@ -10,5 +10,7 @@ userRouter.put('/users/block/:email', blockUser);
 userRouter.put('/users/unblock/:email', unblockUser);
 userRouter.post('/google-login', googleLogin)
 userRouter.post('/send-otp', sendOTP);
+userRouter.post('/verify-otp', verifyOTP);
+
 
 export default userRouter;
