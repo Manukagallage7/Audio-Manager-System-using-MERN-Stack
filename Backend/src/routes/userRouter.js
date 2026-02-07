7aed064cc5a5132a734587a1befacd79ed5abe69
Manukagallage7 <manukakavinda1110@gmail.com>
2026-02-07 16:16:21 +0530

    feat: add Google login and OTP sending routes to userRouter

--- Backend/src/routes/userRouter.js
+++ Backend/src/routes/userRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registerUser, loginUser, getAllUsers, blockUser, unblockUser } from '../controllers/userController.js';
+import { registerUser, loginUser, getAllUsers, blockUser, unblockUser, sendOTP, googleLogin } from '../controllers/userController.js';
 
 const userRouter = express.Router();
 
@@ -8,5 +8,7 @@ userRouter.post('/login', loginUser);
 userRouter.get('/users', getAllUsers);
 userRouter.put('/users/block/:email', blockUser);
 userRouter.put('/users/unblock/:email', unblockUser);
+userRouter.post('/google-login', googleLogin)
+userRouter.post('/send-otp', sendOTP);
 
 export default userRouter;
